a6dbd7b226485c94921f1c0cd7016889d8223d49
CAHL <81452973+cesaraugustoherrreralozano@users.noreply.github.com>
2025-05-04 19:52:15 -0500

    landing page v.0.0.0

---
+++ js/script.js
@@ -0,0 +1,6 @@
+// Envío del formulario de contacto
+document.getElementById('formularioContacto').addEventListener('submit', function(e) {
+    e.preventDefault();
+    alert('¡Gracias por tu mensaje! Te contactaremos pronto.');
+  });
+  
