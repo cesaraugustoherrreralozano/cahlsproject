b16510b1a86f3ea508929d522c17d4fbe21eb70c
CAHL <81452973+cesaraugustoherrreralozano@users.noreply.github.com>
2025-05-04 20:44:29 -0500

    Actualización tailwind 2

--- js/script.js
+++ js/script.js
@@ -1,6 +1,24 @@
-// Envío del formulario de contacto
-document.getElementById('formularioContacto').addEventListener('submit', function(e) {
-    e.preventDefault();
-    alert('¡Gracias por tu mensaje! Te contactaremos pronto.');
-  });
-  
+// script.js
+
+const themeToggleButton = document.getElementById("theme-toggle");
+
+themeToggleButton.addEventListener("click", () => {
+    // Comprobamos si el tema actual es 'dark'
+    if (document.documentElement.classList.contains("dark")) {
+        // Si está en dark mode, lo cambiamos a light mode
+        document.documentElement.classList.remove("dark");
+        localStorage.setItem("theme", "light");
+    } else {
+        // Si está en light mode, lo cambiamos a dark mode
+        document.documentElement.classList.add("dark");
+        localStorage.setItem("theme", "dark");
+    }
+});
+
+// Al cargar la página, verificamos si el usuario tiene configurado un tema
+const currentTheme = localStorage.getItem("theme");
+if (currentTheme === "dark") {
+    document.documentElement.classList.add("dark");
+} else {
+    document.documentElement.classList.remove("dark");
+}
